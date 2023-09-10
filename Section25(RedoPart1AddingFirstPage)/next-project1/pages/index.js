dca214dd85081dfa95732f598ed9da27c3e2ea7d
mrylraihan <mr.ylraihan@gmail.com>
2023-09-09 20:33:52 -0400

    next practice

---
+++ Section25(RedoPart1AddingFirstPage)/next-project1/pages/index.js
@@ -0,0 +1,13 @@
+import Test from '@/components/Test'
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className='app'>
+          <h1>The Home Page</h1>
+          <Test/>
+    </div>
+  )
+}
+
+export default HomePage
