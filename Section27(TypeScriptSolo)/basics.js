a519ee58a70c90a6f81bbf1af711f80f67b5987c
mrylraihan <mr.ylraihan@gmail.com>
2024-04-15 22:01:08 -0700

    typescript practice

--- Section27(TypeScriptSolo)/basics.js
+++ Section27(TypeScriptSolo)/basics.js
@@ -3,3 +3,21 @@ console.log(title);
 // title = 3
 title = "mink";
 console.log(title);
+var numList;
+// a array that only accepts numbers
+var obj;
+var objTest;
+objTest = {
+    id: 1
+};
+console.log(objTest);
+;
+var objTest2 = {
+    id: 2,
+};
+console.log(objTest2);
+var objTest3 = {
+    id: 4,
+    title: "with types"
+};
+console.log(objTest3);
--- Section27(TypeScriptSolo)/basics.ts
+++ Section27(TypeScriptSolo)/basics.ts
@@ -13,3 +13,36 @@ let obj:{
     lastName:"";
     getFullName:()=>void;
 }
+
+
+let objTest :{
+    id:number;
+};
+
+objTest = {
+    id:1
+}
+console.log(objTest)
+
+interface objectStruture {
+    id:number;
+};
+
+let objTest2 :objectStruture= {
+    id:2,
+}
+
+console.log(objTest2)
+
+type objectType = {
+    id:number;
+};
+type titleType = {
+    title:string;
+};
+
+let objTest3 : objectType & titleType={
+    id:4,
+    title:"with types"
+}
+console.log(objTest3)
